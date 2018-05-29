7f1979328ea2b268910277cb9f3117e2f57f7f4e
haolun <haolun@example.com>
2018-05-30 05:12:52 +0800

    travis-ci ready

---
+++ ci.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+echo -e "\n>>>>>>>>>> ---------- custom, override options ---------- >>>>>>>>>>"
+export CI_OPT_SITE_PATH_PREFIX="ci-and-cd"
+export CI_OPT_GITHUB_SITE_REPO_OWNER="home1-oss"
+export CI_OPT_GPG_KEYNAME="59DBF10E"
+export CI_OPT_SONAR_ORGANIZATION="home1-oss-github"
+export CI_OPT_SONAR="true"
+echo -e "<<<<<<<<<< ---------- custom, override options ---------- <<<<<<<<<<\n"
+
+
+echo -e "\n>>>>>>>>>> ---------- call remote script ---------- >>>>>>>>>>"
+if [ -z "${CI_OPT_CI_SCRIPT}" ]; then CI_OPT_CI_SCRIPT="https://github.com/ci-and-cd/maven-build/raw/master/src/main/ci-script/lib_ci.sh"; fi
+echo "WORK_DIR=$(basename $(pwd)) mkdir -p /tmp/${WORK_DIR}; curl -s -L ${CI_OPT_CI_SCRIPT} > /tmp/${WORK_DIR}/lib_ci.sh && source /tmp/${WORK_DIR}/lib_ci.sh"
+WORK_DIR=$(basename $(pwd)) mkdir -p /tmp/${WORK_DIR}; curl -s -L ${CI_OPT_CI_SCRIPT} > /tmp/${WORK_DIR}/lib_ci.sh && source /tmp/${WORK_DIR}/lib_ci.sh
+echo -e "<<<<<<<<<< ---------- call remote script ---------- <<<<<<<<<<\n"
