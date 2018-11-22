eeb80aef4b72ca617c6c5242a838ca21d6a209eb
haolun zhang <opensource@infra.top>
2018-11-23 02:00:45 +0800

    upgrade parent

--- ci.sh
+++ ci.sh
@@ -11,12 +11,12 @@ echo -e "\n>>>>>>>>>> ---------- default options ---------- >>>>>>>>>>"
 if [[ -z ${CI_INFRA_OPT_GIT_PREFIX+x} ]]; then CI_INFRA_OPT_GIT_PREFIX="https://gitlab.com"; fi
 
 
-if [[ -z ${CI_OPT_CI_SCRIPT+x} ]]; then CI_OPT_CI_SCRIPT="${CI_INFRA_OPT_GIT_PREFIX}/ci-and-cd/maven-build/raw/v1.0.2/src/main/ci-script/lib_ci.sh"; fi
+if [[ -z ${CI_OPT_CI_SCRIPT+x} ]]; then CI_OPT_CI_SCRIPT="${CI_INFRA_OPT_GIT_PREFIX}/ci-and-cd/maven-build/raw/v1.0.3/src/main/ci-script/lib_ci.sh"; fi
 if [[ -z ${CI_OPT_GITHUB_SITE_PUBLISH+x} ]]; then CI_OPT_GITHUB_SITE_PUBLISH="false"; fi
 if [[ -z ${CI_OPT_GITHUB_SITE_REPO_OWNER+x} ]]; then CI_OPT_GITHUB_SITE_REPO_OWNER="ci-and-cd"; fi
 if [[ -z ${CI_OPT_GPG_KEYNAME+x} ]]; then CI_OPT_GPG_KEYNAME="59DBF10E"; fi
 if [[ -z ${CI_OPT_INFRASTRUCTURE+x} ]]; then CI_OPT_INFRASTRUCTURE="opensource"; fi
-if [[ -z ${CI_OPT_MAVEN_BUILD_REPO+x} ]]; then CI_OPT_MAVEN_BUILD_REPO="${CI_INFRA_OPT_GIT_PREFIX}/ci-and-cd/maven-build/raw/v1.0.2"; fi
+if [[ -z ${CI_OPT_MAVEN_BUILD_REPO+x} ]]; then CI_OPT_MAVEN_BUILD_REPO="${CI_INFRA_OPT_GIT_PREFIX}/ci-and-cd/maven-build/raw/v1.0.3"; fi
 if [[ -z ${CI_OPT_MAVEN_EFFECTIVE_POM+x} ]]; then CI_OPT_MAVEN_EFFECTIVE_POM="false"; fi
 if [[ -z ${CI_OPT_ORIGIN_REPO_SLUG+x} ]]; then if [[ -n "${CI_PROJECT_PATH}" ]]; then CI_OPT_ORIGIN_REPO_SLUG="ci-and-cd/deps-spring-boot"; else CI_OPT_ORIGIN_REPO_SLUG="ci-and-cd/deps-spring-boot"; fi; fi
 if [[ -z ${CI_OPT_SITE+x} ]]; then CI_OPT_SITE="true"; fi
